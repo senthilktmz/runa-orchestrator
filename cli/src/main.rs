11d2963337213f3f4346202b8841062fe8a22cf3
Senthil Kumar T <senthilktmz@outlook.com>
2024-12-20 21:20:58 +0530

    adding basic white bar down in the editor

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+
+
+
+use crossterm::{
+    execute,
+    style::{Color, SetBackgroundColor, Print, ResetColor},
+    terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, size},
+    cursor::MoveTo,
+};
+
+use std::io::{stdout, Write};
+
+fn main() -> std::io::Result<()> {
+    let mut stdout = stdout();
+
+    execute!(stdout, crossterm::terminal::EnterAlternateScreen)?;
+
+    loop {
+        execute!(stdout, Clear(ClearType::All))?;
+
+        let terminal_size = size()?;
+
+        let width = terminal_size.0;
+        let height = terminal_size.1;
+
+        execute!(
+            stdout,
+            MoveTo(0, height-1),
+            SetBackgroundColor(Color::White),
+            Print(" ".repeat(width as usize)),
+            ResetColor
+        )?;
+
+        stdout.flush()?;
+        break;
+    }
+
+
+
+    Ok(())
+        
 }
+
