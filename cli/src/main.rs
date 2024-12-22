41c0b6d6eb9cb253cd5534b6e8e73b987b252252
t1 <senthilktmz@outlook.com>
2024-12-23 00:51:02 +0530

    adding encrypt and to b64 string test

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,37 @@
+mod aes_gcm_util;
+use aes_gcm_util::{encrypt, decrypt};
+use crate::aes_gcm_util::aes_gcm_key_from_string_literal;
+use base64::{engine::general_purpose, Engine};
+
 fn main() {
-    println!("Hello, world!");
+    encrypt_then_to_b64_string_test();
+}
+
+fn encrypt_then_to_b64_string_test() {
+    println!("Running test {:?}", "encrypt_then_to_b64_string_test");
+    let payload = r#"{
+    "request_params": {
+        "request_type": "command_execution",
+        "command_params": {
+            "command_type": "run_bash_script",
+            "run_mode": "async",
+            "command_data": {
+                "run_bash_script_data": {
+                    "script_data": "ZWNobyAnSGVsbG8sIFdvcmxkIScK",
+                    "script_data_type": "bash_script_b64_utf8"
+                }
+            },
+            "command_progress_info_params": {
+                "stream_progress_type": "realtime"
+            }
+        }
+    }
+}"#;
+
+    let associated_data = b"sksks";
+    let plaintext = payload.as_bytes();
+    let key  = aes_gcm_key_from_string_literal(b"0123456789abcdef0123456789abcdef");
+    let (ciphered, nonce) = encrypt(&key, payload.as_bytes(), associated_data);
+    let b64_string = general_purpose::STANDARD.encode(ciphered);
+    println!("Encrypted data as b64 : {:?}", b64_string);
 }
