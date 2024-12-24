6429fd847793aeff9b6b36de08b5371230e9375a
Senthil Kumar T <senthilktmz@outlook.com>
2024-12-24 14:37:01 +0530

    removing unused files

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,5 @@
 
-use crate::cipher_item::client_encrypt_and_server_decrypt_test;
-use runautils::cipher_item;
+use runautils::cipher_item::{client_encrypt_and_server_decrypt_test};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     client_encrypt_and_server_decrypt_test()?;
