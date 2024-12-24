36be0d56450ef75d94afcfe94b89c2be5d4a9712
Senthil Kumar T <senthilktmz@outlook.com>
2024-12-24 14:30:25 +0530

    moving cipher_item to new git repo

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,17 +1,6 @@
-//use base64::{engine::general_purpose, Engine};
-//use hex;
-//use serde::{Deserialize, Serialize};
-//use std::error::Error;
-//
-//use aes_gcm::aead::consts::U12;
-//use aes_gcm::aead::{Aead, Payload};
-//use aes_gcm::{
-//    aead::{AeadCore, KeyInit, OsRng},
-//    Aes256Gcm, Key, Nonce,
-//};
-//
-mod cipher_item;
+
 use crate::cipher_item::client_encrypt_and_server_decrypt_test;
+use runautils::cipher_item;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     client_encrypt_and_server_decrypt_test()?;
