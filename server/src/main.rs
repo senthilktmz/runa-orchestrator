b458a6aaa6f6ebec04615b16a8940e912356c86c
Senthil Kumar T <senthilktmz@outlook.com>
2024-12-24 15:37:30 +0530

    adding POST support

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,11 @@
 use std::future::Future;
 use std::pin::Pin;
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{guard::Method, http::Method, web, App, HttpResponse, HttpServer, Responder};
 
 struct Route {
     path: &'static str,
-    handler: fn() -> Pin<Box<dyn Future<Output =HttpResponse >>>
+    handler: fn() -> Pin<Box<dyn Future<Output =HttpResponse >>>,
+    request_type: Method,
 }
 
 async fn health() -> HttpResponse {
@@ -15,21 +16,48 @@ fn boxed_health() -> Pin<Box<dyn Future<Output = HttpResponse >>> {
     Box::pin(health())
 }
 
+async fn post_req(req_body: String) -> HttpResponse {
+    HttpResponse::Ok().json(serde_json::json!({
+        "received": req_body
+    }))
+}
+
+fn boxed_post_req() -> Pin<Box<dyn Future<Output = HttpResponse>>> {
+    println!("osososos");
+    Box::pin(post_req("".to_string()))
+}
+
 const ROUTES_LIST: &[Route] = &[
     Route{
         path: "/health",
+        request_type: Method::GET,
         handler: boxed_health
     },
+    Route {
+        path: "/post_req",
+        request_type: Method::POST,
+        handler: boxed_post_req
+    }
 ];
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Starting server");
     HttpServer::new(|| {
-        let tmpApp = ROUTES_LIST.iter().fold(App::new(), |app, route| {
-            app.route(route.path,  web::get().to(route.handler))
+        let tmp_app = ROUTES_LIST.iter().fold(App::new(), |app, route| {
+            match route.request_type {
+                Method::GET => app.route(route.path, web::get().to(route.handler)),
+                Method::POST => app.route(route.path, web::post().to(|body:String| async move {
+                    post_req(body).await
+                })),
+                _ => app,
+            }
         });
-        tmpApp
-    }).bind("127.0.0.1:8080")?.run().await
+
+        tmp_app
+    }).bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
 
 
