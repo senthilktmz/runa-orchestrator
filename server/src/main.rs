96ce4bffcafe5b935e76d62f38e0458aa3bbdbfd
Senthil Kumar T <senthilktmz@outlook.com>
2024-12-26 11:18:13 +0530

    adding conditional compile

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,24 @@
 mod orchestrator;
 mod orchestrator_routes;
 
-use runautils::actix_server_util::{serve_requests, Route};
-use orchestrator::generic_handlers::{boxed_get_req, boxed_post_handler};
-use orchestrator::health_calls::boxed_health;
-use orchestrator::ws_handle_task_request;
-use orchestrator::task_agent;
+use runautils::actix_server_util::{serve_requests};
 
-
-
+#[cfg( feature =  "server_type_orchestrator")]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let routes = orchestrator_routes::routes();
     serve_requests(routes).await
 }
+
+#[cfg(feature = "server_type_task_agent")]
+fn main() {
+    println!("Worker Server");
+}
+
+#[cfg(not(any(feature = "server_type_orchestrator", feature = "server_type_task_agent")))]
+fn main() {
+    println!("Unknown server type");
+}
+
+//
+//
