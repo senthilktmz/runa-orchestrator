fa46486d12af7abb36f36f37c91b7d05ba0d5d79
Senthil Kumar T <senthilktmz@outlook.com>
2024-12-28 23:42:13 +0530

    removing server dir and cli dir

--- cli/src/main.rs
+++
@@ -1,9 +0,0 @@
-
-use runautils::cipher_item::{client_encrypt_and_server_decrypt_test};
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    client_encrypt_and_server_decrypt_test()?;
-    Ok(())
-}
-
-//
