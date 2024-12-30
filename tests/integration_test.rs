67ae1a21cc409bdee16745a4339a28a8715681e7
Senthil Kumar T <senthilktmz@outlook.com>
2024-12-30 21:33:04 +0530

    moving http request send with encryption to runautils

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,12 +1,4 @@
-use reqwest::blocking::Client;
-use runautils::cipher_item::{encrypt_payload, get_decrypted_payload};
-
-
-fn to_json_literal_string(payload: &str) -> String {
-    let escaped_payload = payload.replace("\"", "\\\"");
-    format!("\"{}\"", escaped_payload)
-}
-
+use runautils::actix_server_util::{post_http_request};
 
 #[test]
 fn test_encrypt_and_decrypt_payload() {
@@ -36,24 +28,4 @@ fn test_encrypt_and_decrypt_payload() {
 
     assert!(response.status().is_success());
 
-}
-
-fn post_http_request(url :&str, plain_text_payload : &str,
-                     key : &[u8; 32],
-                     associated_data : &[u8] ) -> reqwest::Result<reqwest::blocking::Response> {
-
-    // Encrypt the payload
-    let encrypted_payload = encrypt_payload(key, plain_text_payload.as_bytes(), associated_data)
-        .expect("Encryption failed");
-
-    let url = "http://127.0.0.1:9191/task_agent";
-    let client = Client::new();
-
-    let formatted_body = to_json_literal_string(encrypted_payload.as_str()); //  format!("\"{}\"", encrypted_payload);
-
-    client
-        .post(url)
-        .header("Content-Type", "application/json")
-        .body(formatted_body) // Wrap in quotes to make it a JSON string
-        .send()
 }
